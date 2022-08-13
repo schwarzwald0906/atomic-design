9482f53a7ad011ffe7e72dd0513204e8ec386821
schwarzwald0906 <93188453+schwarzwald0906@users.noreply.github.com>
2022-08-13 11:53:45 +0900

    header修正

--- src/components/atoms/layout/Header.jsx
+++ src/components/atoms/layout/Header.jsx
@@ -1,3 +1,10 @@
+import { Link } from "react-router-dom";
+
 export const Header = () => {
-  return <Header></Header>;
+  return (
+    <header>
+      <Link to="/">HOME</Link>
+      <Link to="/">USERS</Link>
+    </header>
+  );
 };
